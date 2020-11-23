15acd80db0e41dca7bf9538a48a39c0c41230fea
Ann Almgren <asalmgren@lbl.gov>
2020-11-22 17:51:08 -0800

    This version is conservative and takes at most 4 iterations to enforce min_density in a contrived MiniSB/inputs.32 case with small_dens set equtl to 1e6

--- Source/TimeStep/Nyx_enforce_minimum_density.cpp
+++ Source/TimeStep/Nyx_enforce_minimum_density.cpp
@@ -19,34 +19,42 @@ compute_mu_for_enforce_min_density(
   amrex::Array4<amrex::Real> const& mu_z,
   const amrex::Real small_dens)
 {
+    // NOTE: This is called on a tile box grown by one, and we have already done a FillBoundary/FillPatch.
+    //       We include ghost cells in our tests for rho > small_dens so that we can create fluxes here
+    //       regardless of which side of the grid boundary has the cell needing fixing.
+
+    // NOTE: We trigger a correction when rho < small_dens, but if this is true, we adjust so that 
+    //       rho == 1.1 * small_dens ... just to give ourselves some breathing room 
+    Real target_density = 1.01 * small_dens;
+
     if (state(i,j,k,URHO) < small_dens)
     {
-        std::cout << "UPDATING " << IntVect(i,j,k) << " WITH INITIAL RHO " << state(i,j,k,URHO) << std::endl;
-        std::cout << " and x neighbors " <<  state(i+1,j,k,URHO) << " " <<  state(i-1,j,k,URHO) << std::endl;
-        std::cout << " and y neighbors " <<  state(i,j+1,k,URHO) << " " <<  state(i,j-1,k,URHO) << std::endl;
-        std::cout << " and z neighbors " <<  state(i,j,k+1,URHO) << " " <<  state(i,j,k-1,URHO) << std::endl;
+        // std::cout << "UPDATING " << IntVect(i,j,k) << " WITH INITIAL RHO " << state(i,j,k,URHO) << std::endl;
+        // std::cout << " and x neighbors " <<  state(i+1,j,k,URHO) << " " <<  state(i-1,j,k,URHO) << std::endl;
+        // std::cout << " and y neighbors " <<  state(i,j+1,k,URHO) << " " <<  state(i,j-1,k,URHO) << std::endl;
+        // std::cout << " and z neighbors " <<  state(i,j,k+1,URHO) << " " <<  state(i,j,k-1,URHO) << std::endl;
 
-        Real total_need  = small_dens - state(i,j,k,URHO);
+        Real total_need  = target_density - state(i,j,k,URHO);
 
-        // We only let each cell contribute 1/6 of its difference from small_dens;
-        //    this guarantees that the redistribution can't make a cell with (rho > small_dens)
-        //    go under small_dens
-        Real avail_from_ihi = amrex::max((state(i+1,j,k,URHO) - small_dens) / 6.0, 0.0);
-        Real avail_from_ilo = amrex::max((state(i-1,j,k,URHO) - small_dens) / 6.0, 0.0);
-        Real avail_from_jhi = amrex::max((state(i,j+1,k,URHO) - small_dens) / 6.0, 0.0);
-        Real avail_from_jlo = amrex::max((state(i,j-1,k,URHO) - small_dens) / 6.0, 0.0);
-        Real avail_from_khi = amrex::max((state(i,j,k+1,URHO) - small_dens) / 6.0, 0.0);
-        Real avail_from_klo = amrex::max((state(i,j,k-1,URHO) - small_dens) / 6.0, 0.0);
+        // We only let each cell contribute 1/6 of its difference from target_density;
+        //    this guarantees that the redistribution can't make a cell with (rho > target_density)
+        //    go under target_density
+        Real avail_from_ihi = amrex::max((state(i+1,j,k,URHO) - target_density) / 6.0, 0.0);
+        Real avail_from_ilo = amrex::max((state(i-1,j,k,URHO) - target_density) / 6.0, 0.0);
+        Real avail_from_jhi = amrex::max((state(i,j+1,k,URHO) - target_density) / 6.0, 0.0);
+        Real avail_from_jlo = amrex::max((state(i,j-1,k,URHO) - target_density) / 6.0, 0.0);
+        Real avail_from_khi = amrex::max((state(i,j,k+1,URHO) - target_density) / 6.0, 0.0);
+        Real avail_from_klo = amrex::max((state(i,j,k-1,URHO) - target_density) / 6.0, 0.0);
 
-        std::cout << " avail x " << avail_from_ihi << " " << avail_from_ilo << std::endl;
-        std::cout << " avail y " << avail_from_jhi << " " << avail_from_jlo << std::endl;
-        std::cout << " avail z " << avail_from_khi << " " << avail_from_klo << std::endl;
+        // std::cout << " avail x " << avail_from_ihi << " " << avail_from_ilo << std::endl;
+        // std::cout << " avail y " << avail_from_jhi << " " << avail_from_jlo << std::endl;
+        // std::cout << " avail z " << avail_from_khi << " " << avail_from_klo << std::endl;
 
         Real total_avail = avail_from_ihi + avail_from_ilo +
                            avail_from_jhi + avail_from_jlo +
                            avail_from_khi + avail_from_klo; 
 
-        std::cout << " total avail / total need " << total_avail << " " << total_need << std::endl;
+        // std::cout << " total avail / total need " << total_avail << " " << total_need << std::endl;
 
         Real fac;
         if (total_need < total_avail)
@@ -61,8 +69,6 @@ compute_mu_for_enforce_min_density(
         Real from_khi = fac * avail_from_khi;
         Real from_klo = fac * avail_from_klo;
 
-        std::cout << " total to be donated " << (from_ihi+from_ilo+from_jhi+from_jlo+from_khi+from_klo) << std::endl;
- 
         if (from_ihi > 0)
             mu_x(i+1,j,k) =  from_ihi / (state(i+1,j,k,URHO) - state(i  ,j,k,URHO));
         if (from_ilo > 0)
@@ -102,8 +108,6 @@ create_update_for_minimum_density(
                              -mu_y(i,j  ,k) * (state(i,j  ,k,n) - state(i,j-1,k,n))
                              +mu_z(i,j,k+1) * (state(i,j,k+1,n) - state(i,j,k  ,n))
                              -mu_z(i,j,k  ) * (state(i,j,k  ,n) - state(i,j,k-1,n));
-            if (n == URHO) std::cout  << "UPDATE CELL " << IntVect(i,j,k) << " " << update(i,j,k,n) << std::endl;
-            // if (n == URHO and i >= 9 and i <=11 and j >= 3 and j <= 5 and k == 6) std::cout  << "UPDATE CELL " << IntVect(i,j,k) << " " << update(i,j,k,n) << std::endl;
         } else {
             update(i,j,k,n) = 0.0;
         }
@@ -128,87 +132,111 @@ Nyx::enforce_minimum_density( MultiFab& S_old, MultiFab& S_new,
 
     MultiFab::RegionTag amrhydro_tag("HydroUpdate_" + std::to_string(level));
 
-    if (S_new.min(Density) < small_dens)
-    {
-
-    const amrex::Real a_half = 0.5 * (a_old + a_new);
-    const amrex::Real a_half_inv = 1 / a_half;
-    const amrex::Real a_oldsq = a_old * a_old;
-    const amrex::Real a_newsq = a_new * a_new;
-    const amrex::Real a_new_inv = 1.0 / a_new;
-    const amrex::Real a_newsq_inv = 1.0 / a_newsq;
-    const amrex::Real dt_a_new    = dt / a_new;
-
     int lnum_spec    = NumSpec;
     int lfirst_spec  = FirstSpec;
     Real lsmall_dens = small_dens;
+  
+    Real cur_time = state[State_Type].curTime();
 
-    // Define face-based coefficients to be defined when enforcing minimum density 
-    //     then used to enjoy the updates of all the other variables
-    MultiFab mu_x(amrex::convert(grids,IntVect(1,0,0)), dmap, 1, 0);
-    MultiFab mu_y(amrex::convert(grids,IntVect(0,1,0)), dmap, 1, 0);
-    MultiFab mu_z(amrex::convert(grids,IntVect(0,0,1)), dmap, 1, 0);
-
-    // Initialize to zero; these will only be non-zero at a face across which density is passed...
-    mu_x.setVal(0.);
-    mu_y.setVal(0.);
-    mu_z.setVal(0.);
-
-    // This will hold the update to each cell due to enforcing minimum density in a conservative way
-    MultiFab update(grids , dmap, S_new.nComp(), 0);
-    update.setVal(0.);
-
-    Real rho_old_min_before = S_old.min(0);
-    Real rho_old_sum_before = S_new.sum(0);
-    Real rho_new_min_before = S_new.min(0);
-    Real rho_new_sum_before = S_new.sum(0);
-
-    for (amrex::MFIter mfi(S_new, amrex::TilingIfNotGPU()); mfi.isValid(); ++mfi) 
+    if (S_new.min(Density) < small_dens)
     {
-        const amrex::Box& bx = mfi.tilebox();
-        auto const& uout = S_new.array(mfi);
-        auto const& mu_x_arr = mu_x.array(mfi);
-        auto const& mu_y_arr = mu_y.array(mfi);
-        auto const& mu_z_arr = mu_z.array(mfi);
+        // We need to define this temporary because S_new only has one ghost cell and we need two.
+        MultiFab Sborder;
+        Sborder.define(grids, S_new.DistributionMap(), S_new.nComp(), 2);
 
-        //Unclear whether this should be part of previous ParallelFor
-        amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept 
+        // Define face-based coefficients to be defined when enforcing minimum density 
+        //     then used to enjoy the updates of all the other variables
+        // The ghost face space is only needed as temp space; we only use "valid" faces...
+        MultiFab mu_x(amrex::convert(grids,IntVect(1,0,0)), dmap, 1, 1);
+        MultiFab mu_y(amrex::convert(grids,IntVect(0,1,0)), dmap, 1, 1);
+        MultiFab mu_z(amrex::convert(grids,IntVect(0,0,1)), dmap, 1, 1);
+
+        Real rho_old_min_before = S_old.min(0);
+        Real rho_old_sum_before = S_old.sum(0);
+
+        Real rho_new_min_before = S_new.min(0);
+        Real rho_new_sum_before = S_new.sum(0);
+
+        Real rho_new_min_after;
+        Real rho_new_sum_after;
+
+        Real rho_new_min = rho_new_min_before;
+
+        bool too_low = (rho_new_min < small_dens);
+
+        int iter = 0;
+
+        // 10 is an arbitrary limit here -- just to make sure we don't get stuck here somehow 
+        while (too_low and iter < 10)
         {
-          compute_mu_for_enforce_min_density(i, j, k, uout, mu_x_arr, mu_y_arr, mu_z_arr, lsmall_dens);
-        });
-    }
+            // First make sure that all ghost cells are updated because we use them in defining fluxes
+            FillPatch(*this, Sborder, 2, cur_time, State_Type, Density, Sborder.nComp());
 
-    for (amrex::MFIter mfi(S_new, amrex::TilingIfNotGPU()); mfi.isValid(); ++mfi) 
-    {
-        const amrex::Box& bx = mfi.tilebox();
-        auto const& uout = S_new.array(mfi);
-        auto const& mu_x_arr = mu_x.array(mfi);
-        auto const& mu_y_arr = mu_y.array(mfi);
-        auto const& mu_z_arr = mu_z.array(mfi);
-        auto const& upd_arr = update.array(mfi);
+            // Initialize to zero; these will only be non-zero at a face across which density is passed...
+            mu_x.setVal(0.);
+            mu_y.setVal(0.);
+            mu_z.setVal(0.);
 
-        //Unclear whether this should be part of previous ParallelFor
-        amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept 
-        {
-          create_update_for_minimum_density(i, j, k, uout, mu_x_arr, mu_y_arr, mu_z_arr, upd_arr, lfirst_spec, lnum_spec);
-        });
-    }
+            // This will hold the update to each cell due to enforcing minimum density in a conservative way
+            MultiFab update(grids , dmap, Sborder.nComp(), 0);
+            update.setVal(0.);
 
-    S_new.plus(update,0,S_new.nComp(),0);
+            for (amrex::MFIter mfi(Sborder, amrex::TilingIfNotGPU()); mfi.isValid(); ++mfi) 
+            {
+                 const amrex::Box& gbx = mfi.growntilebox(1);
+                 auto const& sbord = Sborder.array(mfi);
+                 auto const& mu_x_arr = mu_x.array(mfi);
+                 auto const& mu_y_arr = mu_y.array(mfi);
+                 auto const& mu_z_arr = mu_z.array(mfi);
 
-    if (S_new.contains_nan())
-       amrex::Abort("NaN in enforce_minimum_density");
+                 //Unclear whether this should be part of previous ParallelFor
+                 amrex::ParallelFor(gbx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept 
+                 {
+                   compute_mu_for_enforce_min_density(i, j, k, sbord, mu_x_arr, mu_y_arr, mu_z_arr, lsmall_dens);
+                 });
+            }
 
-    Real rho_new_min_after = S_new.min(0);
-    Real rho_new_sum_after = S_new.sum(0);
+            for (amrex::MFIter mfi(Sborder, amrex::TilingIfNotGPU()); mfi.isValid(); ++mfi) 
+            {
+                // Only update on valid cells
+                const amrex::Box& bx = mfi.tilebox();
+                auto const& sbord_arr = Sborder.array(mfi);
+                auto const& mu_x_arr  = mu_x.array(mfi);
+                auto const& mu_y_arr  = mu_y.array(mfi);
+                auto const& mu_z_arr  = mu_z.array(mfi);
+                auto const& upd_arr   = update.array(mfi);
 
-    amrex::Print() << "MIN OF rho_old / rho_new / new rho_new " << 
-        rho_old_min_before << " " << rho_new_min_before << " " << rho_new_min_after << std::endl;
-    amrex::Print() << "SUM OF rho_old / rho_new / new rho_new " << 
-        rho_old_sum_before << " " << rho_new_sum_before << " " << rho_new_sum_after << std::endl;
+                amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept 
+                {
+                  create_update_for_minimum_density(i, j, k, sbord_arr, 
+                                                    mu_x_arr, mu_y_arr, mu_z_arr, upd_arr, 
+                                                    lfirst_spec, lnum_spec);
+                });
+            }
 
-    if (rho_new_min_after < small_dens)
-       amrex::Abort("Not able to enforce small_dens this way after all");
+            S_new.plus(update,0,S_new.nComp(),0);
 
+            if (S_new.contains_nan())
+               amrex::Abort("NaN in enforce_minimum_density");
+
+            rho_new_min_after = S_new.min(0);
+
+            amrex::Print() << "After " << iter+1 << " iterations " << std::endl;
+            amrex::Print() << "  MIN OF rho_old / rho_new / new rho_new " << 
+                rho_old_min_before << " " << rho_new_min_before << " " << rho_new_min_after << std::endl;
+
+            too_low = (rho_new_min_after < small_dens);
+            iter++;
+
+        } // iter
+
+        rho_new_sum_after = S_new.sum(0);
+
+        amrex::Print() << "After " << iter << " iterations " << std::endl;
+        amrex::Print() << "  SUM OF rho_old / rho_new / new rho_new " << 
+            rho_old_sum_before << " " << rho_new_sum_before << " " << rho_new_sum_after << std::endl;
+
+        if (rho_new_min_after < small_dens)
+           amrex::Abort("Not able to enforce small_dens this way after all");
     }
 }
